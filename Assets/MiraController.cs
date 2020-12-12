af54152ec68c69d0dff62f06e7642d3aca5e6c6f
Wine Silva <winny.silva@yahoo.com.br>
2020-12-11 21:30:08 -0300

    tilesets e comportamento da mira

---
+++ Assets/MiraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiraController : MonoBehaviour
+{
+    public int forcaImpulso = 20;
+
+    private FixedJoint2D joint;
+    private float forcaQuebra;
+    // Start is called before the first frame update
+    void Start()
+    {
+        joint = this.GetComponent<FixedJoint2D>();
+        forcaQuebra = joint.breakForce;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (TryGetComponent<FixedJoint2D>(out joint))
+            {
+                Debug.Log("fire"); 
+                Rigidbody2D rg = joint.connectedBody;
+                Destroy(joint);
+
+                float graus = Mathf.Abs(rg.rotation);
+
+                graus = graus / 85;
+
+                rg.AddForce(new Vector3(graus * forcaImpulso, (1-graus) * forcaImpulso), ForceMode2D.Impulse);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/AtiradorController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtiradorController : MonoBehaviour
+{
+    public float speed = 5;
+    private float velocidade;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float graus = 85 * (horizontalInput );
+        
+        Debug.Log(graus);
+        Quaternion rot = Quaternion.Euler(0, 0, -graus);
+        
+        this.transform.rotation = rot;
+
+    }
+
+
+}
