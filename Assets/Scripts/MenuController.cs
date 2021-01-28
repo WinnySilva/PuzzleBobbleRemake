d7c0206e0e1f50259d02c530806ac5f54eb55798
Fernando <vzs.fernando@gmail.com>
2021-01-27 23:58:28 -0300

    Trocar de cena do menu para o jogo

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene("Scenes/fase1"); //trocar de cena
+        }
+    }
+}
